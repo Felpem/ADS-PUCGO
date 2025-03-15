8fe64a4d22ff42449a4b75f8973fb5882ac2adab
Felpem <magalhaesf581@gmail.com>
2025-03-15 01:44:09 -0300

    .

---
+++ LAB_PROG/Estruturas Sequenciais/Belas_Artes_Day.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Belas_Artes_Day {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        double dia, idade, ingresso;
+        System.out.print("Dia: ");
+        dia = leia.nextDouble();
+        System.out.print("Idade: ");
+        idade = leia.nextDouble();
+        System.out.print("Ingresso: ");
+        ingresso = leia.nextDouble();
+            if (idade <= 14 && dia == 3){
+                ingresso = (ingresso/2) * 0.5;
+            }
+            else{
+                ingresso = ingresso / 2;
+            }
+        System.out.printf("valor ingresso R$" + ingresso);
+    }
+}
--- LAB_PROG/Estruturas Sequenciais/IMC_Honesto.java
+++ LAB_PROG/Estruturas Sequenciais/IMC_Honesto.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-public class exerc2 {
+public class IMC_Honesto {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         double imc, altura, peso, m;
+        String mensgimc = "";
         System.out.print("Peso:");
         peso = sc.nextDouble();
         System.out.print("Altura");
@@ -10,23 +11,23 @@ public class exerc2 {
         m = altura / 100;
         imc = peso /(m * m);
         if (imc < 18.5){
-            System.err.println("Abaixo do peso");
+            mensgimc = ("Abaixo do peso");
         }
         else if (imc > 18.5 && imc < 24.9) {
-           System.err.println("Peso normal"); 
+           mensgimc = ("Peso normal"); 
         }
         else if (imc > 25 && imc < 29.9) {
-            System.err.println("Sobrepeso");
+            mensgimc = ("Sobrepeso");
         }
         else if (imc > 30 && imc < 34.9) {
-            System.err.println("Obesidade Leve");
+            mensgimc = ("Obesidade Leve");
         }
         else if (imc > 35 && imc < 39.9) {
-            System.err.println("Obesidade moderada");
+            mensgimc = ("Obesidade moderada");
         }
         else if (imc > 40) {
-            System.err.println("Obesidade Mórbida");
+            mensgimc = ("Obesidade Mórbida");
         }
-
+        System.out.println(mensgimc);
     }
 }
